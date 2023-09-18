fea2bd3a422696d71f99e79f1443730491b5bafc
Joey Blake <jblake@hubspot.com>
2023-09-18 10:25:48 -0400

    node esm needs an .mjs extension, this creates it and updates the import

--- scripts/build-module.js
+++ scripts/build-module.js
@@ -2,21 +2,22 @@ const esbuild = require( 'esbuild' );
 const name    = 'react-splide';
 
 
-async function buildScript( format = 'esm' ) {
+async function buildScript( format = 'esm', ext = 'js' ) {
   return esbuild.build( {
     entryPoints: [ `./src/js/index.ts` ],
     bundle     : true,
-    outfile    : `./dist/js/${ name }.${ format }.js`,
+    outfile    : `./dist/js/${ name }.${ ext }`,
     format,
     external   : [ 'react', 'react-dom' ],
-    target     : [ 'es2019' ],
+    target     : [ 'es2019' ]
   } );
 }
 
 async function buildModule() {
   return Promise.all( [
-    buildScript(),
-    buildScript( 'cjs' ),
+    buildScript( 'esm', 'esm.js' ),
+    buildScript( 'esm', 'esm.mjs' ),
+    buildScript( 'cjs', 'cjs.js' ),
   ] );
 }
 
